0a76af3742e1e1a691e679f0841c67df6f21981b
Jacob <jacob1107@163.com>
2022-09-12 16:44:28 +0800

    init

---
+++ src/com/atguigu/uml/Person.java
@@ -0,0 +1,14 @@
+package com.atguigu.uml;
+
+public class Person{ 
+	@SuppressWarnings("unused")
+	private Integer id;
+	private String name;
+	public void setName(String name){
+		this.name=name;
+	}
+	public String getName(){
+		return  name;
+	}
+}
+
